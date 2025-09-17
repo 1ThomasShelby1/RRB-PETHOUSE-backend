cd77af8f132ea942fed07250db265b59cb446948
Rutik Bhamare <115616775+1ThomasShelby1@users.noreply.github.com>
2025-09-17 21:57:08 +0530

    vishal

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173", // Use deployed frontend on Vercel
+  origin:"http://localhost:5173", // Use deployed frontend on Vercel
   credentials: true, // Allow cookies/credentials
   optionsSuccessStatus: 200, // Handle legacy browsers
 };
